0616549f067ed4f83a5d3d3dd57d08c23f768729
Your Name <judith.lenz@outlook.de>
2023-11-02 16:54:28 +0100

    text and questions added

--- script.js
+++ script.js
@@ -31,4 +31,45 @@ let questions = [
     answer_4: "Nixdorf 820",
     right_answer: 1,
   },
+  {
+    question: "Welches Attribut kann man NICHT für die Textarea verwenden?",
+    answer_1: "readonly",
+    answer_2: "max",
+    answer_3: "from",
+    answer_4: "spellcheck",
+    right_answer: 1,
+  },
+  {
+    question:
+      "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem Attribut title aus?",
+    answer_1: "a[title]{...}",
+    answer_2: "a > title {...}",
+    answer_3: "a.title {...}",
+    answer_4: "a=title {...}",
+    right_answer: 1,
+  },
+  {
+    question: "Wie defniert man in JavaScript eine Variable?",
+    answer_1: "let 100 = rate;",
+    answer_2: "100 = let rate;",
+    answer_3: "rate = 100;",
+    answer_4: "let rate = 100;",
+    right_answer: 4,
+  },
 ];
+
+let currentQuestion = 0;
+
+function init() {
+  document.getElementById("all-questions").innerHTML = questions.length;
+  showQuestion();
+}
+
+function showQuestion() {
+  let question = questions[currentQuestion];
+  document.getElementById('questiontext').innerHTML = question['question'];
+  document.getElementById('answer_1').innerHTML = question['answer_1'];
+  document.getElementById('answer_2').innerHTML = question['answer_2'];
+  document.getElementById('answer_3').innerHTML = question['answer_3'];
+  document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
