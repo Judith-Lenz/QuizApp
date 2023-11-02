e2bafb9a75b48217b1910a5801bed6c6dd4303e2
Your Name <judith.lenz@outlook.de>
2023-11-02 15:06:30 +0100

    JSON added in JS

---
+++ script.js
@@ -0,0 +1,34 @@
+let questions = [
+  {
+    question: "Wer hat HTML erfunden?",
+    answer_1: "Robbie Williams",
+    answer_2: "Lady Gaga",
+    answer_3: "Tim Berners-Lee",
+    answer_4: "Justin Bieber",
+    right_answer: 3,
+  },
+  {
+    question: "Was bedeutet das HTML Tag &lt;a&gt;?",
+    answer_1: "Text Fett",
+    answer_2: "Container",
+    answer_3: "Ein Link",
+    answer_4: "Kursiv",
+    right_answer: 3,
+  },
+  {
+    question: "Wie bindet man eine Website in eine Website ein?",
+    answer_1: "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+    answer_2: "&lt;iframe&gt;",
+    answer_3: "&lt;frame&gt;",
+    answer_4: "&lt;frameset&gt;",
+    right_answer: 2,
+  },
+  {
+    question: "Wie hieß der erste HomePC?",
+    answer_1: "Altair 8800",
+    answer_2: "ENIAC",
+    answer_3: "Douglas C. Engelbart",
+    answer_4: "Nixdorf 820",
+    right_answer: 1,
+  },
+];
