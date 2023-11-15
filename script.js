021d17a86c09a1d99f68bcaa7561688e2cc2c105
Your Name <judith.lenz@outlook.de>
2023-11-15 10:27:45 +0100

    progress bar

--- script.js
+++ script.js
@@ -69,22 +69,30 @@ function init() {
 
 function showQuestion() {
   if (currentQuestion >= questions.length) {
-    // TODO Show Endscreen
     document.getElementById("endScreen").style = "";
     document.getElementById("questionBody").style = "display: none;";
     document.getElementById("amount-of-questions").innerHTML = questions.length;
-    document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
-    document.getElementById('header-image').src = 'img/brain result.png';
+    document.getElementById("amount-of-right-questions").innerHTML =
+      rightQuestions;
+    document.getElementById("header-image").src = "img/brain result.png";
   } else {
-  }
+    // show question
 
-  let question = questions[currentQuestion];
-  document.getElementById("question-number").innerHTML = currentQuestion + 1;
-  document.getElementById("questiontext").innerHTML = question["question"];
-  document.getElementById("answer_1").innerHTML = question["answer_1"];
-  document.getElementById("answer_2").innerHTML = question["answer_2"];
-  document.getElementById("answer_3").innerHTML = question["answer_3"];
-  document.getElementById("answer_4").innerHTML = question["answer_4"];
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById("progess-bar").innerHTML = `${percent} %`;
+    document.getElementById("progess-bar").style = `width:${percent}%`;
+
+    console.log("Fortschritt:", percent);
+
+    let question = questions[currentQuestion];
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
@@ -92,7 +100,8 @@ function answer(selection) {
   let selectedAnswerNumber = selection.slice(-1);
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
-  if (selectedAnswerNumber == question["right_answer"]) { // Richtige Frage beantwortet
+  if (selectedAnswerNumber == question["right_answer"]) {
+    // Richtige Frage beantwortet
     document.getElementById(selection).parentNode.classList.add("bg-success");
     rightQuestions++;
   } else {
